420c2e45a4456755b2a1d55e98a52728727935b8
istTsvetelin <91822090+istTsvetelin@users.noreply.github.com>
2021-10-06 00:01:55 +0300

    updating

--- BitOperationsAndTypes/miniMarket.c
+++ BitOperationsAndTypes/miniMarket.c
@@ -11,7 +11,7 @@ int main (){
 
     unsigned int uiUserChoise = 0;
     float fAmount = 0.0;
-    double dMoneyOwned ;
+    double dMoneyOwned = 0 ;
     printf("1 - tomato (%.2lf lv.) for 1kg\n",dTomatoPrice);
     printf("2 - flour (%.2lf lv.) for 1 kg\n",dFlourPrice);
     printf("3 - yogurt (%.2lf lv.) each",dYogurtPrice);
@@ -20,19 +20,53 @@ int main (){
     printf("6 - lolipop (%.2lf lv.) each\n",dLolipopPrice);
     printf("9 - to exit\n");
     printf("Enter what you want :\n");
-    scanf("%d",uiUserChoise);
+    scanf("%d", &uiUserChoise);
     if(uiUserChoise > 0 && uiUserChoise < 7)
     {
         printf("Enter amount :\n");
-        scanf("%f",fAmount);
+        scanf("%f", &fAmount);
     }else{
         return 0;
     }
    
-    while(uiUserChoise != 9){
+    while(uiUserChoise != 9)
+    {
         switch(uiUserChoise){
+            case 1:
 
+            break;
+
+            case 2:
+
+            break;
+
+            case 3:
+
+            break;
+            case 4:
+
+            break;
+            case 5:
+
+            break;
+
+            case 6:
+
+            break;
+
+            default:
+                printf("Not a valid selection. Try again .");
+            break;
         }
+        printf("Enter product code:\n");
+        scanf("%d",&uiUserChoise);
+        while(fAmount <= 0){
+            printf("Enter amount:\n");
+            scanf("%d", &fAmount);
+            if(fAmount <=0)
+                printf("Please enter positve amount.\n");
+        }
+       
     }
 
 
